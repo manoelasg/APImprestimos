b244f0f127c0728f75715f1729696b1fc98f9129
Alex Monteiro <alexcda@gmail.com>
2017-09-26 21:19:07 -0300

    Estrutura do Usuario Parte II

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const UsuarioSchema = require('../schemas/usuario');
+const jwt = require('jsonwebtoken');
+const passwordHash = require('password-hash');
+const expressJwt = require('express-jwt');
 
 let router = express.Router();
 
 router.get('/', (request, response) => {
     console.log('get /usuario');
+});
+
+router.post('/cadastro', (request, response) => {
+    console.log('post  /usuario');
+    response.send(request.body)
 });
 
 module.exports = router;

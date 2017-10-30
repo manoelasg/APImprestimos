bd335fba45734d0b73f1853396c2ed507b1ca2d4
Manoela <manoela_sgomes@hotmail.com>
2017-10-30 17:57:53 -0200

    Incluindo 'strict mode' para eliminar erro com versão do node

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passwordHash = require('password-hash');
-const jwt = require('jsonwebtoken');
-const expressJwt = require('express-jwt');
-const usuariosController = require('./controllers/usuarios');
-const consultaController = require('./controllers/consulta');
-// const emprestimosController = require('./controllers/emprestimos');
+require('strict-mode')(function () {
+    const express = require('express');
+    const mongoose = require('mongoose');
+    const bodyParser = require('body-parser');
+    const passwordHash = require('password-hash');
+    const jwt = require('jsonwebtoken');
+    const expressJwt = require('express-jwt');
+    const usuariosController = require('./controllers/usuarios');
+    const consultaController = require('./controllers/consulta');
+    // const emprestimosController = require('./controllers/emprestimos');
 
-//gera aplicação
-const app = express();
+    //gera aplicação
+    const app = express();
 
-//middlewares
-app.use(bodyParser.json());
-app.use('/usuarios', usuariosController);
-app.use('/consulta', consultaController);
 
-//conecta ao bd
-mongoose.connect('mongodb://localhost/apimprestimos');
+    //middlewares
+    app.use(bodyParser.json());
+    app.use('/usuarios', usuariosController);
+    app.use('/consulta', consultaController);
 
-//servidor
-app.listen(3000, () => {
-    console.log('Servidor inicializado na porta 3000');
+
+    //conecta ao bd
+    mongoose.connect('mongodb://localhost/apimprestimos');
+
+    //servidor
+    app.listen(3000, () => {
+        console.log('Servidor inicializado na porta 3000');
+
+    });
 });
